57b1d9a7146cf392e7e4042de80534ead459237b
masinlocandher-max <masinlocandher@gmail.com>
2026-09-04 23:35:05 +0800

    feat: route institutional admissions and student app flow

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AppLayout from './components/AppLayout'
 import Home from './pages/Home'
+import Apply from './pages/Apply'
 import EntranceExam from './pages/EntranceExam'
 import Exam from './pages/Exam'
 import Learner from './pages/Learner'
+import Programs from './pages/Programs'
+import Payment from './pages/Payment'
+import AccountSetup from './pages/AccountSetup'
+import StudentApp from './pages/StudentApp'
+import AdmissionsReview from './pages/AdmissionsReview'
 import './styles.css'
 import './learner.css'
+import './admissions.css'
 import '../brand/code/cognita-brand.css'
 import './brand-runtime.css'
 
@@ -17,9 +24,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route element={<AppLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/apply" element={<Apply />} />
           <Route path="/learner" element={<Learner />} />
           <Route path="/entrance-exam" element={<EntranceExam />} />
           <Route path="/entrance-exam/start" element={<Exam />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/account-setup" element={<AccountSetup />} />
+          <Route path="/app" element={<StudentApp />} />
+          <Route path="/operations/admissions" element={<AdmissionsReview />} />
         </Route>
       </Routes>
     </BrowserRouter>
